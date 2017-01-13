b835b4912ddf2acc906a27aa6f9ae258be264108
Sascha Goldhofer <sascha.goldhofer@sueddeutsche.de>
2017-01-13 13:39:18 +0100

    Add project

---
+++ README.md
@@ -0,0 +1,68 @@
+# withObservable
+
+> Small and simple observable pattern.
+
+- enforces event-name registration on init
+- observable pattern is used to clearly distinct from EventEmitter and sometimes ambigious _on_-methods
+- lightweight solution
+
+
+## Usage
+
+
+```js
+
+// mixin the observable behaviour:
+// myobject.observers;
+// myobject.addObserver;
+// myobject.addObserverOnce;
+// myobject.removeObserver;
+// myobject.notify;
+myObject = withObservable.call(myObject, ["event_name", "another_event"]);
+
+// register to an event
+myObject.addObserver("event_name", function () {
+	// do something
+});
+
+// register to an event once
+myObject.addObserver("event_name", function () {
+	// do something, will never be called again
+});
+
+// stop observing given callback
+myObject.addObserver("event_name", callback);
+
+// register event (should be used within myobject)
+myObject.notify("event_name", "and", "params");
+
+// contains callbacks by events
+myObject.observers
+
+```
+
+
+## Recommendations
+
+Within a Constructor use the mixin as follows
+
+```
+AClass.UPDATE_EVENT = "update";
+AClass.END_EVENT = "end";
+
+function AClass() {
+    withObservable.call(this, [AClass.UPDATE_EVENT, AClass.END_EVENT]);
+}
+```
+
+This ensures that
+
+- The event names are accessible from outside
+- refactoring of eventnames is simple
+- better overview of actual events
+- calling withObservable in constructor prevents conflicts
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,95 @@
+/* eslint no-invalid-this: 0 */
+"use strict";
+
+
+function notify() {
+    var event = Array.prototype.shift.apply(arguments);
+    var iter;
+    var count;
+    var os;
+
+    if (this.observers[event] == null) {
+        throw new Error("Unknown event " + event + " for observable (" + Object.keys(this.observers).join(",") + ")");
+    } else if (this.observers[event].length === 0) {
+        return false;
+    }
+
+    os = this.observers[event];
+    for (iter = 0, count = os.length; iter < count; iter += 1) {
+        os[iter].apply(os[iter], arguments);
+    }
+
+    return true;
+}
+
+function addObserver(event, observer) {
+    if (this.observers[event] == null) {
+        throw new Error("Invalid event " + event + " for observable");
+    } else if (typeof observer === "function") {
+        this.observers[event].push(observer);
+    }
+
+    return this;
+}
+
+function removeObserver(event, observer) {
+    if (this.observers[event] == null) {
+        return;
+    }
+
+    // reminder: creates a new array (garbage collector)
+    this.observers[event] = this.observers[event].filter(function removeObs(registeredObserver) {
+        return registeredObserver !== observer;
+    });
+}
+
+function addObserverOnce(event, observer) {
+    var self = this;
+
+    return addObserver.call(this, event, function once() {
+        removeObserver.call(self, event, once);
+        observer.apply(this, arguments);
+    });
+}
+
+function registerEvent(event) {
+    if (Array.isArray(event)) {
+        registerEvent.apply(this, event);
+    } else {
+        for (var i = 0, length = arguments.length; i < length; i += 1) {
+            this[arguments[i]] = [];
+        }
+    }
+}
+
+// eslint-disable-next-line no-use-before-define
+withObservable.dispose = function dispose() {
+    delete this.observers;
+    delete this.addObserver;
+    delete this.addObserverOnce;
+    delete this.removeObserver;
+    delete this.notify;
+};
+
+function withObservable(eventList) {
+    var observers = {};
+
+    if (arguments.length === 0 || eventList.length === 0) {
+        throw new Error("Missing eventList for observable");
+    }
+
+    for (var i = 0, length = arguments.length; i < length; i += 1) {
+        registerEvent.call(observers, arguments[i]);
+    }
+
+    this.observers = observers;
+    this.addObserver = addObserver;
+    this.addObserverOnce = addObserverOnce;
+    this.removeObserver = removeObserver;
+    this.notify = notify;
+
+    return this;
+}
+
+
+module.exports = withObservable;
